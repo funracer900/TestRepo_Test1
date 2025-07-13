a8fff17edbe3fd16fd030645005f59dabd41b2a2
funracer900 <bardo.lang@t-online.de>
2025-07-13 12:35:20 +0200

    Add 2D array creation and sorting output to main.py

--- main.py
+++ main.py
@@ -9,5 +9,13 @@ x = np.array([1, -4, 3, 6, 9, -40, 20, 2], dtype=np.int32)
 for elem in sorted(x):
     print(elem)
 
+# Create a 2-dimensional array of signed integers
+x2d = np.array([[1, -4, 3], [6, 9, -40]], dtype=np.int32)
+print("2D array:")
+print(x2d)
+
+for row in x2d:
+    for elem in sorted(row):
+        print(elem)
 
 print("that's all of 1st try!")
