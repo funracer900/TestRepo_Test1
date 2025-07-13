a860f0fb593aa1351e8ae267b2645d5530132e57
funracer900 <bardo.lang@t-online.de>
2025-07-13 15:38:30 +0200

    Enhance visualization: Add histogram plotting for array values in main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 print('1st try...')
 
 x = np.array([1, -4, 3, 6, 9, -40, 20, 2], dtype=np.int32)
+
+#plt.plot(x, marker='o', linestyle='--', color='b')
+plt.hist( x, bins=10, color='blue', alpha=0.7, edgecolor='black')
+plt.title('1st try')
+plt.xlabel('Index')
+plt.ylabel('Value')
+plt.grid(True)
+plt.show()
 #print(x)
 
 
