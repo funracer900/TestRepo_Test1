ff222b0839e779261229a0afc217110a8f56ffd8
funracer900 <bardo.lang@t-online.de>
2025-07-13 10:08:39 +0200

    Refactor main.py: Update print statements and modify array values for improved output

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import numpy as np
 
-print('Hello World!')
+print('1.st try...')
 
-x = np.array([1, 4, 3, 6])
-print('Goodbye World!')
-print(x)
+x = np.array([1, -4, 3, 6, 9,-40,20,2], dtype=np.int32)
+#print(x)
+
+
+for elem in sorted(x):
+    print(elem)
+
+print('that´s all of  1.st try!')
