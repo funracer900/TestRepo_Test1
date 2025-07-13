8591712175d05177418accc338766906092e0924
funracer900 <bardo.lang@t-online.de>
2025-07-13 10:26:14 +0200

    Fix typos in print statements and add workspace configuration file

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import numpy as np
 
-print('1.st try...')
+print('1st try...')
 
-x = np.array([1, -4, 3, 6, 9,-40,20,2], dtype=np.int32)
+x = np.array([1, -4, 3, 6, 9, -40, 20, 2], dtype=np.int32)
 #print(x)
 
 
 for elem in sorted(x):
     print(elem)
 
-print('that´s all of  1.st try!')
+
+print("that's all of 1st try!")
